de2b01be4d1771f4a247d87d9eb4bee2e8b9dacf
trustpizza <maxelolsson@gmail.com>
2022-08-21 09:24:10 -0500

    Create addition, subtraction, multiplication, and division func

---
+++ scripts.js
@@ -0,0 +1,19 @@
+function addition(a,b) {
+    return a + b;
+}
+
+function subtraction(a,b) {
+    return a - b;
+}
+
+function multiplication(a,b) {
+    return a * b;
+}
+
+function division(a,b) {
+    return a / b;
+}
+
+function operate(operator, a, b) {
+    return operator(a, b);
+}
